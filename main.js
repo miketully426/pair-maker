6320101fa4b09820cbde96e6ad9cb68bf8d8f4e9
miketully426 <fake@no.org>
2025-10-22 15:23:56 -0500

    created basic pair list

---
+++ main.js
@@ -0,0 +1,35 @@
+// take a list of users
+// iterate through the list to make pairs
+// remove a user when put into a pair
+// add any odd user to a pair to a three
+// finally print out all pairs
+
+const names = [
+  "Mike",
+  "Jyoti",
+  "Consuella",
+  "Rome",
+  "Tai",
+  "Marcella",
+  "Becca",
+];
+const pairList = [];
+do {
+  const pair = Array(2);
+  pair[0] = getRandomName(names);
+  pair[1] = getRandomName(names);
+  pairList.push(pair);
+} while (names.length > 1);
+
+if (names.length === 1) {
+  pairList[0].push(names[0]);
+}
+
+console.log(pairList);
+
+function getRandomName(nameArray) {
+  const randomNumber = Math.floor(Math.random() * nameArray.length);
+  const outputName = nameArray[randomNumber];
+  nameArray.splice(randomNumber, 1);
+  return outputName;
+}
